e69286d4f40ce0aafe238e3ba11afea2a06a3d02
Firealarm-freak <mike.stegne.host@gmail.com>
2016-04-16 09:14:21 -0400

    Added in some of the setup function, corrected some errors in the configuration bits, and added in a call of the setup function from the main method.

--- PIC Audio Player.X/main.c
+++ PIC Audio Player.X/main.c
@@ -2,8 +2,8 @@
  *  PIC Audio Player - A simple single chip solution to playing small audio files  *
  *  Created by mikemadealarms on April 13, 2016 at 3:20 PM                         *
  * ------------------------------------------------------------------------------- *
- *  Last modification by mikemadealarms on April 13, 2016 at 3:25 PM               *
- *  Last modification made was: Add in basic components and routines.              *
+ *  Last modification by mikemadealarms on April 16, 2016 at 9:12 AM               *
+ *  Last modification made was: Made some minor changes to the setup function      *
  ***********************************************************************************/
 
 #include <xc.h>
@@ -14,7 +14,7 @@
 
 //CONFIG1 Register
 #pragma config FOSC = INTOSC   //Set the PIC MCU to use the internal oscillator as a clock source.
-#pragma config WTDE = OFF      //Disable the watch-dog timer as it is not necessary.
+#pragma config WDTE = OFF      //Disable the watch-dog timer as it is not necessary.
 #pragma config PWRTE = OFF     //Disable the power up timer as it is not necessary.
 #pragma config MCLRE = ON      //Enable the MCLRE function linked to PORTA3 on the PIC MCU.
 #pragma config CP = OFF        //Disable code protection as this is completely pointless for this application.
@@ -26,7 +26,7 @@
 
 //CONFIG2 Register
 #pragma config WRT = OFF    //Disable the flash re-write protection as it is completely pointless for this application.
-#pragma config PPLEN = OFF  //Disable the internal 4x PLL multiplier as a high frequency clock is not needed.
+#pragma config PLLEN = OFF  //Disable the internal 4x PLL multiplier as a high frequency clock is not needed.
 #pragma config STVREN = ON  //Enable the stack under/over flow reset function on the PIC MCU.
 #pragma config BORV = LO    //Set the point at which the brown out reset is triggered to the low trip point.
 #pragma config LVP = OFF    //Disable the low voltage programming feature as I have a high voltage programmer.
@@ -38,12 +38,23 @@
 //Setup Function, initializes the MCU and prepares it for the various tasks it will perform.
 void setup() {
     //Configure Timing Related Modules
-    OSCCON = 0x60;  //Set the internal oscillator to run at 2MHz and disable the internal 4x PLL.
+    OSCCON = 0x70;      //Set the internal oscillator to run at 8MHz and disable the internal 4x PLL.
+    OPTION_REG = 0x5F;  //Enable the use of the 20K ohm internal pull-up resistors and enable Timer0.
+    
+    //Configure IO Related Modules and IO Ports
+    TRISA = 0x39;   //Set PORTA1 and PORTA2 to outputs and leave the rest as inputs.
+    ANSELA = 0x00;  //Set all of PORTA to digital IO rather than analog inputs.
+    WPUA = 0x01;    //Enable the 20K ohm internal weak pull-up resistor on PORTA0.
+    LATA = 0x00;    //Clear LATA so none of PORTA is in the logic high state.
+    
+    
 }
 
 //Main Function, called upon reset of the PIC MCU.
 void main() {
-    return;
+    setup();  //Call setup function to initialize the PIC MCU.
+    
+    //Run in a continuous loop until the end of time (Or the PIC MCU is reset).
 }
 
 /****************
